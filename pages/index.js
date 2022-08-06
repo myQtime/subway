4fc366a0a22195568824f9dc06743f2588bd353b
myQtime <myqtimelife@gmail.com>
2022-08-07 02:27:02 +0800

    css

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ export default function home() {
             <IgBlock />
             <Play />
             <Notice />
-            <Footer />
+            {/* <Footer /> */}
         </div>
     )
 }
