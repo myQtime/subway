27d9924a760f536a561bc01e78908b707d94aee4
myQtime <myqtimelife@gmail.com>
2022-10-02 17:32:59 +0800

    Update food.js

--- component/Buttons/food.js
+++ component/Buttons/food.js
@@ -35,7 +35,7 @@ export default function food(props) {
                     {props.data
                         ? props.data.map((item) => (
                               <div
-                                  className="food mt-2 mx-1 pe-auto"
+                                  className="food mt-2 mx-1 mx-md-2 pe-auto"
                                   key={item.id}
                                   onClick={(e) => {
                                       setActive(e)
